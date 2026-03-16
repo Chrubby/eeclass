5b01f29e66d15d22a2106036e764d2da0810ad91
ccwangg <angelawang0557@gmail.com>
2026-03-16 23:05:28 +0800

    merge

---
+++ frontend/src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Course from '../views/Course.vue'
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/login" },
+    { path: '/dashboard', name: 'Home', component: Home },
+    { path: '/course/:id', name: 'Course', component: Course,  // :id 代表課程代碼 
+      children: [
+        {
+          path: '', 
+          name: 'CourseAnnouncement',
+          component: () => import('../views/course/Announcement.vue') 
+        },
+        {
+          path: 'material',
+          name: 'CourseMaterial',
+          component: () => import('../views/course/Material.vue')
+        },
+        {
+          path: 'homework',
+          name: 'CourseHomework',
+          component: () => import('../views/course/Homework.vue')
+        },
+        {
+          path: 'exam',
+          name: 'CourseExam',
+          component: () => import('../views/course/Exam.vue')
+        },
+        {
+          path: 'grade',
+          name: 'CourseGrade',
+          component: () => import('../views/course/Grade.vue')
+        }
+      ]
+    },
+    { path: "/login", name: 'Login', component: Login },
+    { path: "/register", name: 'Register', component: Register }
+  ]
+})
+
+export default router
